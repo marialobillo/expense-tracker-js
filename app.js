06afa3dcff694905fc0a573edf180ff8302a2eea
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-12-16 17:32:10 +0100

    refactoring

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
     // initial array of expenses, reading from localStorage
     const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
 
-    document.getElementById('expForm').addEventListener('submit', function(e){
+    document.getElementById('exp-Form').addEventListener('submit', function(e){
 
         e.preventDefault();
 
@@ -27,52 +27,74 @@
         // localStorage 
         localStorage.setItem('expenses', JSON.stringify(expenses));
 
-        document.getElementById('expForm').reset();
+        document.getElementById('exp-Form').reset();
         showExpenses();
     });
 
     function showExpenses(){
 
         const expenseTable = document.getElementById('expenseTable');
-
         expenseTable.innerHTML = '';
 
-        for(let i = 0; i < expenses.length; i++){
+        if(expenses.length > 0){
+            for(let i = 0; i < expenses.length; i++){
 
-            const expenseRowEl = document.createElement('TR');
-            expenseTable.appendChild(expenseRowEl);
+                expenseTable.appendChild(createDataRow(expenses[i])); 
+                
+            }  // end of for loop
+        }  else {
+            // expenses count is 0
+            expenseTable.appendChild(createEmptyRow());           
+        }
+    }
 
-            const expenseTdTypeEl = document.createElement('TD');
-            expenseTdTypeEl.textContent = expenses[i].type;
-            expenseRowEl.appendChild(expenseTdTypeEl);
+    function createEmptyRow(){
+        const expenseRowEl = document.createElement('TR');       
 
-            const expenseTdNameEl = document.createElement('TD');
-            expenseTdNameEl.textContent = expenses[i].name;
-            expenseRowEl.appendChild(expenseTdNameEl);
+        const expenseTdTypeEl = document.createElement('TD');
+        expenseTdTypeEl.setAttribute('colspan', 5);
+        expenseTdTypeEl.textContent = 
+            'No expense items yet! Please add one up top...';
+        expenseRowEl.appendChild(expenseTdTypeEl);
 
-            const expenseTdDateEl = document.createElement('TD');
-            expenseTdDateEl.textContent = expenses[i].date;
-            expenseRowEl.appendChild(expenseTdDateEl);
+        return expenseRowEl;
+    }
 
-            const expenseTdAmountEl = document.createElement('TD');
-            expenseTdAmountEl.textContent = '$' + expenses[i].amount;
-            expenseRowEl.appendChild(expenseTdAmountEl);
+    function createDataRow(expense){
+        
+        const expenseRowEl = document.createElement('TR');
 
-            const expenseTdOptionsEl = document.createElement('TD');
-            const deleteAnchorEl = document.createElement('A');
-            deleteAnchorEl.className = "deleteButton";
-            deleteAnchorEl.onclick = function(e){
-                deleteExpense(expenses[i].id);
+        const expenseTdTypeEl = document.createElement('TD');
+        expenseTdTypeEl.textContent = expense.type;
+        expenseRowEl.appendChild(expenseTdTypeEl);
 
-                 // localStorage
-                localStorage.setItem('expenses', JSON.stringify(expenses));
-                showExpenses();
-            }
+        const expenseTdNameEl = document.createElement('TD');
+        expenseTdNameEl.textContent = expense.name;
+        expenseRowEl.appendChild(expenseTdNameEl);
 
-            deleteAnchorEl.textContent = 'Delete';
-            expenseRowEl.appendChild(deleteAnchorEl);
+        const expenseTdDateEl = document.createElement('TD');
+        expenseTdDateEl.textContent = expense.date;
+        expenseRowEl.appendChild(expenseTdDateEl);
+
+        const expenseTdAmountEl = document.createElement('TD');
+        expenseTdAmountEl.textContent = '$' + expense.amount;
+        expenseRowEl.appendChild(expenseTdAmountEl);
+
+        const expenseTdOptionsEl = document.createElement('TD');
+        const deleteAnchorEl = document.createElement('A');
+        deleteAnchorEl.className = "deleteButton";
+        deleteAnchorEl.onclick = function(e){
+            deleteExpense(expense.id);
+
+                // localStorage
+            localStorage.setItem('expenses', JSON.stringify(expenses));
+            showExpenses();
         }
 
+        deleteAnchorEl.textContent = 'Delete';
+        expenseRowEl.appendChild(deleteAnchorEl);
+
+        return expenseRowEl;
     }
 
     function deleteExpense(id){
